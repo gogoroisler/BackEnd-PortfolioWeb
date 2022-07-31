3264fd239cb6b9a6e67a7a08c3c2ce2394236b75
gogoroisler <santiago.gonzalez.roisler@gmail.com>
2022-07-31 12:37:14 -0300

    cors

--- src/main/java/com/portfolioweb/sgr/SgrApplication.java
+++ src/main/java/com/portfolioweb/sgr/SgrApplication.java
@@ -2,6 +2,8 @@ package com.portfolioweb.sgr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SgrApplication {
@@ -9,5 +11,16 @@ public class SgrApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SgrApplication.class, args);
 	}
-
+public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/persona").allowedOrigins("https://portfolio-web-argprog.web.app");
+                                registry.addMapping("/explaboral").allowedOrigins("https://portfolio-web-argprog.web.app");
+                                registry.addMapping("/educacion").allowedOrigins("https://portfolio-web-argprog.web.app");
+                                registry.addMapping("/proyecto").allowedOrigins("https://portfolio-web-argprog.web.app");
+                                registry.addMapping("/auth").allowedOrigins("https://portfolio-web-argprog.web.app");
+			}
+		};
+	}
 }
